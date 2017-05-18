f583a582dc19a5d908d3e411c92b983b95de5c78
takosuke.yokohama <username@example.com>
2017-05-18 16:44:31 +0900

    FistCommit

---
+++ hello.js
@@ -0,0 +1,18 @@
+var Hello = (function () {
+    function Hello() {
+        this.count = 0;
+        console.log('Hello World');
+    }
+    Hello.prototype.tick = function () {
+        ++this.count;
+        console.log('count', this.count);
+    };
+    return Hello;
+}());
+var hello = new Hello();
+function main() {
+    hello.tick();
+    setTimeout(main, 1000);
+}
+main();
+//# sourceMappingURL=hello.js.map
